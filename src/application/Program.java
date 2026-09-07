171e882b760e3063a533618883b193b132bb52a2
Eliel Guedes <elielguedes817@gmail.com>
2026-09-07 13:28:01 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4000.00, dep);
 		sellerdao.insert(newSeller);
 		System.out.println("Inserted! new id = "+ newSeller.getId());
+		
+		System.out.println("==== TEST 5: SELLER Update ====");
+		seller = sellerdao.findById(1);
+		seller.setName("Maria waine");
+		sellerdao.update(seller);
+		System.out.println("Update completed");
+
 	}
 
 }
